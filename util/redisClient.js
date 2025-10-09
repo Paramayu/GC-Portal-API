9a56848fa8c5f73a6a3b52464284ad3844419f49
Paramayu Bhattarai <bparamayu@gmail.com>
2025-10-09 17:40:42 +0545

    Setup of Cloud Redis Server

--- util/redisClient.js
+++ util/redisClient.js
@@ -2,7 +2,14 @@ let redisClient;
 (async () => {
   const { createClient } = require("redis");
 
-  redisClient = createClient();
+  redisClient = createClient({
+    username: "default",
+    password: "bZryesSIKJPChEE6bVOitUvaKqmgfRTO",
+    socket: {
+      host: "redis-14410.crce206.ap-south-1-1.ec2.redns.redis-cloud.com",
+      port: 14410,
+    },
+  });
 
   redisClient.on("error", err => console.error("Redis Client Error"));
 
